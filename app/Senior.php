c8d79f3155aa23b69d0f281d671cd6945e1d3fce
Arifia Kasastra R <arka.progammer@gmail.com>
2020-07-01 14:44:57 +0700

    Create model and table for Senior

---
+++ app/Senior.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property integer $id
+ * @property int $idsenior
+ * @property int $idusroh
+ * @property int $idkamar
+ * @property string $nama
+ * @property string $nim
+ * @property string $jeniskelamin
+ * @property string $foto
+ * @property string $username
+ * @property string $password
+ * @property string $passcode
+ * @property string $tahunajaran
+ * @property string $token
+ * @property string $rememberme
+ * @property string $created_at
+ * @property string $updated_at
+ */
+class Senior extends Model
+{
+    /**
+     * The table associated with the model.
+     * 
+     * @var string
+     */
+    protected $table = 'senior';
+
+    /**
+     * The "type" of the auto-incrementing ID.
+     * 
+     * @var string
+     */
+    protected $keyType = 'integer';
+
+    /**
+     * @var array
+     */
+    protected $fillable = ['idsenior', 'idusroh', 'idkamar', 'nama', 'nim', 'jeniskelamin', 'foto', 'username', 'password', 'passcode', 'tahunajaran', 'token', 'rememberme', 'created_at', 'updated_at'];
+
+}
---
+++ database/migrations/2020_07_01_073433_create_senior_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSeniorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('senior', function (Blueprint $table) {
+            $table->id();
+            $table->integer('idsenior');
+            $table->integer('idusroh');
+            $table->integer('idkamar');
+            $table->string('nama');
+            $table->string('nim');
+            $table->string('jeniskelamin');
+            $table->string('foto');
+            $table->string('username');
+            $table->string('password');
+            $table->string('passcode');
+            $table->string('tahunajaran');
+            $table->string('token');
+            $table->string('rememberme');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('senior');
+    }
+}
