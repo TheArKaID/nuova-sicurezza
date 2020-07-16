655e9ec410f71315f5cb4bb4811b18115acc16fd
Arifia Kasastra R <arka.progammer@gmail.com>
2020-07-16 20:16:14 +0700

    Create Model and Table Kamar

---
+++ app/Kamar.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property integer $id
+ * @property int $idtahun
+ * @property string $nomor
+ * @property boolean $lantai
+ * @property string $gedung
+ * @property string $created_at
+ * @property string $updated_at
+ */
+class Kamar extends Model
+{
+    /**
+     * The table associated with the model.
+     * 
+     * @var string
+     */
+    protected $table = 'kamar';
+
+    /**
+     * The "type" of the auto-incrementing ID.
+     * 
+     * @var string
+     */
+    protected $keyType = 'integer';
+
+    /**
+     * @var array
+     */
+    protected $fillable = ['idtahun', 'nomor', 'lantai', 'gedung', 'created_at', 'updated_at'];
+
+}
---
+++ database/migrations/2020_07_16_130806_create_kamar_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKamarTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kamar', function (Blueprint $table) {
+            $table->id();
+            $table->integer('idtahun');
+            $table->string('nomor');
+            $table->tinyInteger('lantai');
+            $table->string('gedung', 10);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kamar');
+    }
+}
