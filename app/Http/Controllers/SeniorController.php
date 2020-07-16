c0c3f3133c5bd99c74bf1f1ca655a211e91ca796
Arifia Kasastra R <arka.progammer@gmail.com>
2020-07-16 13:59:40 +0700

    Check if Senior using Mobile or Not

--- app/Http/Controllers/SeniorController.php
+++ app/Http/Controllers/SeniorController.php
@@ -11,13 +11,18 @@ use App\Pengaturan;
 
 class SeniorController extends Controller
 {
+    protected $mobile;
+
     public function __construct()
     {
+        $this->mobile = new \Helper;
         $this->middleware('auth:senior');
     }
 
     public function index()
     {
+        if($this->mobile->isMobile())
+            return view('m.senior.dashboard');
         return view('senior.dashboard');
     }
 
