c19d4d65fbf36a4bf77680c4aa806765882100c3
Arifia Kasastra R <arka.progammer@gmail.com>
2020-07-22 00:38:00 +0700

    Redirect / to /s temporary

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return redirect("/s");
 });
 
 Route::get('/s/login', "Auth\SeniorAuthController@login")->name('senior.login');
