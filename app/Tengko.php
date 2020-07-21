d4bfa751521aa27e9858a11b8c0c961ff3862418
Arifia Kasastra R <arka.progammer@gmail.com>
2020-07-21 23:29:36 +0700

    Create Tengko Model and Table

---
+++ app/Tengko.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property integer $id
+ * @property int $idtahun
+ * @property string $tipe
+ * @property string $penjelasan
+ * @property string $created_at
+ * @property string $updated_at
+ */
+class Tengko extends Model
+{
+    /**
+     * The table associated with the model.
+     * 
+     * @var string
+     */
+    protected $table = 'tengko';
+
+    /**
+     * The "type" of the auto-incrementing ID.
+     * 
+     * @var string
+     */
+    protected $keyType = 'integer';
+
+    /**
+     * @var array
+     */
+    protected $fillable = ['idtahun', 'tipe', 'penjelasan', 'created_at', 'updated_at'];
+
+}
