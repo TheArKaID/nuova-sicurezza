1d2cb3117477518db091fd8da58031eea3454b89
Arifia Kasastra R <arka.progammer@gmail.com>
2020-10-06 20:14:06 +0700

    Add information to About section

--- resources/views/m/senior/help/about.blade.php
+++ resources/views/m/senior/help/about.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.senior-m')
 
+@section('styles')
+<style>
+    img {
+        width: inherit;
+        height: inherit;
+    }
+    p {
+        text-align: justify;
+        text-justify: inter-word;
+    }
+</style>
+@endsection
+
 @section('content')
     <div class="row">
         <div class="col-lg-12">
@@ -20,7 +33,31 @@
     <div class="row">
         <div class="col-lg-12">
             <div class="main-card mb-3 card">
-                
+                <div class="card-body"><h5 class="card-title">Tentang Sicurezza</h5>
+                    <div class="scroll-area-md">
+                        <div class="scrollbar-container ps--active-y ps">
+                            <p>
+                                Sicurezza berasal dari bahasa Itali, yang berarti Keamanan.
+                                Pada tahun 2019, sebuah aplikasi dibuat dan dikembangkan untuk memudahkan 
+                                Divisi Keamanan dalam melakukan pencatatan perpoinan oleh Arifia Kasastra R 
+                                dan Muhammad Miftah, yang kemudian diberi nama Sicurezza. Aplikasi ini adalah 
+                                versi ke 2 yang telah dikembangkan dari Aplikasi sebelumnya
+                            </p>
+                            <p>
+                                Dalam aplikasi ini, SR dan ASR dapat melihat data Resident, Kamar, Usroh serta 
+                                SR ASR yang ada. SR ASR juga dimudahkan dalam proses pencatatan perpoinan yang 
+                                dapat direkap oleh Divisi Keamanan.
+                            </p>
+                            <p>
+                                Namun Demikian, Aplikasi ini jauh dari kata sempurna, sehingga kritik dan saran
+                                akan sangat diharapkan dari teman-teman SR dan ASR sekalian.
+                            </p>
+                            <div class="ps__rail-x" style="left: 0px; bottom: -360px;">
+                                <div class="ps__thumb-x" tabindex="0" style="left: 0px; width: 0px;"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
