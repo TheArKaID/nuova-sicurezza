a31769ddd4b1d395dc927cc45719022a047245d8
Arifia Kasastra R <arka.progammer@gmail.com>
2020-10-06 20:50:52 +0700

    Add Contact Information to Contact section

--- resources/views/m/senior/help/contact.blade.php
+++ resources/views/m/senior/help/contact.blade.php
@@ -20,8 +20,64 @@
     <div class="row">
         <div class="col-lg-12">
             <div class="main-card mb-3 card">
-                
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">
+                        <div class="widget-content p-0">
+                            <div class="widget-content-wrapper">
+                                <div class="card-body">
+                                    Apabila anda memiliki Pertanyaan, Kritik ataupun Saran, 
+                                    silahkan menghubungi nomor di bawah ini
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list-group-item" data-url="6285712141966">
+                        <div class="widget-content p-0">
+                            <div class="widget-content-wrapper">
+                                <div class="widget-content-left mr-3">
+                                    <div class="widget-content-left">
+                                        <i class="rounded-circle fa fa-user fa-2x" width="52"></i>
+                                    </div>
+                                </div>
+                                <div class="widget-content-left flex2">
+                                    <div class="widget-heading">Arifia Kasastra R</div>
+                                    <div class="widget-subheading opacity-10">
+                                        <span><b class="text-success">+62 857 1214 1966</b> (WA)</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list-group-item" data-url="6289691908367">
+                        <div class="widget-content p-0">
+                            <div class="widget-content-wrapper">
+                                <div class="widget-content-left mr-3">
+                                    <div class="widget-content-left">
+                                        <i class="rounded-circle fa fa-user fa-2x" width="52"></i>
+                                    </div>
+                                </div>
+                                <div class="widget-content-left flex2">
+                                    <div class="widget-heading">Muhammad Miftah</div>
+                                    <div class="widget-subheading opacity-10">
+                                        <span><b class="text-primary">+62 896 9190 8367</b> (WA))</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
+@endsection
+
+@section('scripts')
+<script>
+    $(document).ready(function($) {
+        $(".list-group-item").click(function() {
+            window.open("https://wa.me/"+ $(this).data("url") +"?text=Assalamu'alaikum", "_blank");
+            loadui();
+        });
+    });
+</script>
 @endsection
