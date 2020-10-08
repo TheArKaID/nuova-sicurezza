48b6762fa86c64ee4a84c0c6741a2ea1a589182a
Arifia Kasastra R <arka.progammer@gmail.com>
2020-10-08 13:59:02 +0700

    Creating auth to the new layout file

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{asset('assets/admin/css/main.css')}}">
+    @laravelPWA
+    <style>
+      .centered {
+            text-align: center;
+            margin: 0;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            -ms-transform: translate(-50%, -50%);
+        }
+    </style>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
--- resources/views/senior/auth/forgot.blade.php
+++ resources/views/senior/auth/forgot.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.auth')
 
 @section('title')
-    Login | Sicurezza
+    Forgot Password
 @endsection
 
 @section('content')
 <div class="col-md-4"></div>
 <div class="col-md-4 centered">
     <div class="card">
-        <div class="card-body"><h5 class="card-title">Login</h5>
+        <div class="card-body"><h5 class="card-title">Reset Password</h5>
             <div>
-                <form class="form-inline" style="display: inline-block" action="{{route('senior.login.post')}}" method="POST">
+                <form class="form-inline" style="display: inline-block" action="{{route('senior.forgotpassword.post')}}" method="POST">
                     {{ csrf_field() }}
                     <div class="form-group">
                         @error('username')
@@ -21,18 +21,15 @@
                         <input name="username" placeholder="Username" type="text" class="col-md-12 mb-2 form-control" style="width: none" required>
                     </div>
                     <div class="form-group">
-                        @error('password')
+                        @error('resettoken')
                             <span class="invalid-feedback" style="display: block" role="alert">
                                 <strong>{{ $message }}</strong>
                             </span>
                         @enderror
-                        <input name="password" placeholder="Password" type="password" class="col-md-12 mb-2 form-control" style="width: none" required>
+                        <input name="resettoken" placeholder="Reset Token" type="number" class="col-md-12 mb-2 form-control" style="width: none" required>
                     </div>
                     <div class="form-group">
-                        <button class="col-md-12 btn btn-primary">Login</button>
-                    </div>
-                    <div class="form-group">
-                        <a href="{{ route('senior.forgotpassword') }}">Lupa Password ?</a>
+                        <button class="col-md-12 btn btn-primary">Reset</button>
                     </div>
                 </form>
             </div>
