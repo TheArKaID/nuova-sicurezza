b5acd3fe498a938b59fbcd58c8934161a649b3f1
Arifia Kasastra R <arka.progammer@gmail.com>
2020-10-16 21:36:57 +0700

    Installing pusher-js and laravel echo

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -16,15 +16,15 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * allows your team to easily build robust real-time web applications.
  */
 
-// import Echo from 'laravel-echo';
+import Echo from "laravel-echo";
 
-// window.Pusher = require('pusher-js');
+window.Pusher = require('pusher-js');
 
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-//     forceTLS: true
-// });
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    cluster: 'ap1',
+    key: 'a7c47123512fbd61abbe'
+});
+
 // import Compress from 'client-compress';
 window.Compress = require('client-compress')
