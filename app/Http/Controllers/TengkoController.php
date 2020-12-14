a91fac2770dc72fefc80172d38ed2fa01409b2f5
Arifia Kasastra R <arka.progammer@gmail.com>
2020-12-14 23:16:17 +0700

    Fix when Senior can view all tengko

--- app/Http/Controllers/TengkoController.php
+++ app/Http/Controllers/TengkoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Helpers\Helper;
 use App\Tengko;
+use Illuminate\Support\Facades\Auth;
 
 class TengkoController extends Controller
 {
@@ -40,9 +41,15 @@ class TengkoController extends Controller
     public function getAll()
     {
         $ta = $this->helper->idTahunAktif();
-        $ringan = Tengko::where('idtahun', $ta)->where('tipe', 'Ringan')->orderBy('poin')->get();
-        $sedang = Tengko::where('idtahun', $ta)->where('tipe', 'Sedang')->orderBy('poin')->get();
-        $berat = Tengko::where('idtahun', $ta)->where('tipe', 'Berat')->orderBy('poin')->get();
+        $ringan = Tengko::where('idtahun', $ta)->where('tipe', 'Ringan')
+            ->where('jeniskelamin', Auth::user()->jeniskelamin)
+            ->orderBy('poin')->get();
+        $sedang = Tengko::where('idtahun', $ta)->where('tipe', 'Sedang')
+            ->where('jeniskelamin', Auth::user()->jeniskelamin)
+            ->orderBy('poin')->get();
+        $berat = Tengko::where('idtahun', $ta)->where('tipe', 'Berat')
+            ->where('jeniskelamin', Auth::user()->jeniskelamin)
+            ->orderBy('poin')->get();
         $tengko = array('ringan' => $ringan, 'sedang' => $sedang, 'berat' => $berat);
         
         return $tengko;
@@ -51,12 +58,15 @@ class TengkoController extends Controller
     public function getSearch($query)
     {
         $ta = $this->helper->idTahunAktif();
-        $ringan = Tengko::where('idtahun', $ta)->where('tipe', 'Ringan')->
-                where('penjelasan', 'LIKE', '%' .$query. '%')->orderBy('poin')->get();
-        $sedang = Tengko::where('idtahun', $ta)->where('tipe', 'Sedang')->
-                where('penjelasan', 'LIKE', '%' .$query. '%')->orderBy('poin')->get();
-        $berat = Tengko::where('idtahun', $ta)->where('tipe', 'Berat')->
-                where('penjelasan', 'LIKE', '%' .$query. '%')->orderBy('poin')->get();
+        $ringan = Tengko::where('idtahun', $ta)->where('tipe', 'Ringan')
+                ->where('penjelasan', 'LIKE', '%' .$query. '%')
+                ->where('jeniskelamin', Auth::user()->jeniskelamin)->orderBy('poin')->get();
+        $sedang = Tengko::where('idtahun', $ta)->where('tipe', 'Sedang')
+                ->where('penjelasan', 'LIKE', '%' .$query. '%')
+                ->where('jeniskelamin', Auth::user()->jeniskelamin)->orderBy('poin')->get();
+        $berat = Tengko::where('idtahun', $ta)->where('tipe', 'Berat')
+                ->where('penjelasan', 'LIKE', '%' .$query. '%')
+                ->where('jeniskelamin', Auth::user()->jeniskelamin)->orderBy('poin')->get();
         $tengko = array('ringan' => $ringan, 'sedang' => $sedang, 'berat' => $berat);
         
         return $tengko;
@@ -65,7 +75,7 @@ class TengkoController extends Controller
     public function getPelanggaran($tipe)
     {
         $ta = $this->helper->idTahunAktif();
-        $tengko = Tengko::where('idtahun', $ta)->where('tipe', $tipe)->get();
+        $tengko = Tengko::where('idtahun', $ta)->where('jeniskelamin', Auth::user()->jeniskelamin)->where('tipe', $tipe)->get();
         $result = array();
         foreach ($tengko as $key => $value) {
             $res['id'] = $value['id'];
